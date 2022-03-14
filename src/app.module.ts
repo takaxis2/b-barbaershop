7ce85c1b30d249df387ae32973dec08f7e8c1c0d
takaxis2 <axis1130@gmail.com>
2022-03-14 23:22:59 +0900

    disabled mysql, port changed

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,16 +6,16 @@ import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type:'mysql',
-      host:'localhost',
-      port:3306,
-      username:'root',
-      password:'1q2w3e4r',
-      database:'barbershop',
-      entities:[],
-      synchronize:true, //얘는 테스트때만
-    }),
+    // TypeOrmModule.forRoot({
+    //   type:'mysql',
+    //   host:'localhost',
+    //   port:3306,
+    //   username:'root',
+    //   password:'1q2w3e4r',
+    //   database:'barbershop',
+    //   entities:[],
+    //   synchronize:true, //얘는 테스트때만
+    // }),
     AuthModule
   ],
   controllers: [AppController],
--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,6 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+
+}
