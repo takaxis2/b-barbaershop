825793f72e4f3cf3b24c1d83a2eddb949a95de3e
takaxis2 <axis1130@gmail.com>
2022-03-16 02:21:37 +0900

    log-in with passport and passport-google-oauth

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,33 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Req, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
 export class AuthController {
 
+    constructor(private readonly authservice: AuthService){};
+
+    @Get()
+    home():string{
+        return "asdasd"
+    }
+
+    @Get("google")
+    @UseGuards(AuthGuard('google'))
+    async googleAuth(@Req() req){
+        console.log('google')
+    }
+
+    @Get('redirect')
+    @UseGuards(AuthGuard('google'))
+    googleAuthRedirect(@Req() req){
+        console.log('redirect');
+        return this.authservice.googleLogin(req);
+    }
+
+
+    @Get("kakao")
+    kakao():string{
+        return "kakao log in";
+    }
 }
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { GoogleStrategy } from './strategies/google.strategy';
+import { PassportModule } from '@nestjs/passport';
 
 @Module({
-  providers: [AuthService],
+  imports:[
+    PassportModule
+  ],
+  providers: [AuthService, GoogleStrategy],
   controllers: [AuthController]
 })
 export class AuthModule {}
